1f3712e81d0f837eada05b52c62217b4f7b95667
ksthakur <kunalsingh28.4@gmail.com>
2021-02-21 00:11:20 +0530

    django rest framework changes

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,6 @@ from .import views
 urlpatterns = [
 	path('', views.home, name="home"),
     path('api/employee_list', views.employee_list, name='employee_list'),
-    path('api/employee_detail/<pk>', views.employee_detail, name='employee_detail'),
+    path('api/employee_detail/<int:pk>', views.employee_detail, name='employee_detail'),
     path('api/employee_filter', views.employee_filter, name='employee_filter')
 ]
--- api/views.py
+++ api/views.py
@@ -1,89 +1,60 @@
 from django.shortcuts import render
 from api.models import Employee
 from .serializer import EmployeeSerializer
-from django.shortcuts import render
-
-from django.http.response import JsonResponse
+from django.http.response import HttpResponse, JsonResponse
 from rest_framework.parsers import JSONParser
 from rest_framework import status
-
 from rest_framework.decorators import api_view
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.response import Response
 # Create your views here.
 
 def home(request):
     context1 = {'employee' : Employee.objects.all()}
     return render(request, 'home_page.html', context1)
 
-@api_view(['GET', 'POST', 'DELETE'])
+@api_view(['GET', 'POST'])
+@csrf_exempt
 def employee_list(request):
     if request.method == 'GET':
         employee_list = Employee.objects.all()
         employee_serializer = EmployeeSerializer(employee_list, many=True)
-        response_data = {
-        	"message": "all employees list",
-            "employee": employee_serializer.data
-        }
-        return JsonResponse(response_data, safe=False)
+        return Response(employee_serializer.data)
 
     elif request.method == 'POST':
-        employee_data = JSONParser().parse(request)
-        employee_serializer = EmployeeSerializer(data=employee_data)
+        employee_serializer = EmployeeSerializer(data=request.data)
         if employee_serializer.is_valid():
             employee_serializer.save()
-        response_data = {
-        	"message": "employee added successfully"
-        }
-        return JsonResponse(response_data, safe=False)
-
-    elif request.method == 'DELETE':
-        response_data = {
-            "message": "this method is not allowed as it delete all employees"
-        }
-        return JsonResponse(response_data, safe=False)
+            return Response(employee_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(employee_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
+@csrf_exempt
 def employee_detail(request, pk):
     try:
         employee = Employee.objects.get(pk=pk)
     except Employee.DoesNotExist:
-        response_data = {
-        	"message": "this employee id is not valid"
-        }
-        return JsonResponse(response_data, safe=False)
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         employee_serializer = EmployeeSerializer(employee)
-        response_data = {
-        	"message": "employee detail",
-            "employee": employee_serializer.data
-        }
-        return JsonResponse(response_data, safe=False)
+        return Response(employee_serializer.data)
 
     elif request.method == 'PUT':
-        employee_data = JSONParser().parse(request)
-        employee_serializer = EmployeeSerializer(employee, data=employee_data)
+        employee_serializer = EmployeeSerializer(employee, data=request.data)
         if employee_serializer.is_valid():
             employee_serializer.save()
-        response_data = {
-        	"message": "employee updated successfully"
-        }
-        return JsonResponse(response_data, safe=False)
+            return Response(employee_serializer.data)
+        return Response(employee_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         employee.delete()
-        response_data = {
-        	"message": "employee deleted successfully"
-        }
-        return JsonResponse(response_data, safe=False)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['GET'])
+@csrf_exempt
 def employee_filter(request):
-    employee_list = Employee.objects.filter(gender='Male')
-
     if request.method == 'GET':
+        employee_list = Employee.objects.filter(gender='Female')
         employee_serializer = EmployeeSerializer(employee_list, many=True)
-        response_data = {
-        	"message": "all employees list with filter gender=male",
-            "employee": employee_serializer.data
-        }
-        return JsonResponse(response_data, safe=False)
+        return Response(employee_serializer.data)
